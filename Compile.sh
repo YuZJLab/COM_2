54d5f21cd425a9e81b0e3e8bb8757910138cf3b5
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2020-03-15 21:14:42 +0800

    New files added without significant change and tons of errors.

--- Compile.sh
+++ Compile.sh
@@ -4,17 +4,23 @@ rm -f *.bbl
 rm -f *.blg
 rm -f *.toc
 rm -f *.out
+rm -f *.tmp
 rm -f *.fls
 rm -f *.brf
+rm -f *.xdv
 rm -f *.fdb_latexmk
 echo Start XeLaTeX......1/5
-xelatex Main.tex
+xelatex -no-pdf Main.tex
 echo BiBTeX......2/5
 bibtex Main
 echo XeLaTeX...... 3/5
-xelatex Main.tex
+xelatex -no-pdf Main.tex
 echo XeLaTeX...... 4/5
+cat Main.tex|sed 's;^\\\documentclass\[draft\,chinese\]{ylbook};\\\documentclass[chinese]{ylbook};'>Main.tmp1.tex
+mv Main.tex Main.tmp.tex
+mv Main.tmp1.tex Main.tex
 xelatex Main.tex
+mv Main.tmp.tex Main.tex
 echo Delete files...... 5/5
 rm -f *.log
 rm -f *.aux
@@ -22,7 +28,9 @@ rm -f *.bbl
 rm -f *.blg
 rm -f *.toc
 rm -f *.out
+rm -f *.tmp
 rm -f *.fls
 rm -f *.brf
+rm -f *.xdv
 rm -f *.fdb_latexmk
 echo Done!
